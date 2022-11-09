a48306dd56985fd165c057ba82728d8ef0834804
Ashmit Yadav <beingashmit@gmail.com>
2022-11-10 03:33:56 +0530

    Navbar

---
+++ zara/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+  <div>
+<Navbar/>
+  </div>
+    </div>
+  );
+}
+
+export default App;
